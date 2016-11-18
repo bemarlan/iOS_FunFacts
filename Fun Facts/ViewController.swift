01b482a43daea94535acc369a81ff3040723393e
Beverly Lanning <beverly_lanning@reyrey.com>
2016-11-18 11:40:12 -0500

    initial commit

---
+++ Fun Facts/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Fun Facts
+//
+//  Created by Lanning, Beverly on 11/17/16.
+//  Copyright © 2016 Beverly Lanning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func showFunFact() {
+        var randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funFactLabel.text = factBook.randomFact()
+    }
+}
+
